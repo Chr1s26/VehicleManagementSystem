985da2a5e8bff2dacebd82cdc33ca6815ee7900e
chris44 <chris44@LAPTOP-USMGRTCT>
2024-08-21 22:07:33 +0700

    edited

---
+++ CarsAndMotorCyclesManagementSystem/src/Model/Customer.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Customer {
+
+	private String customerName;
+	private String customerId;
+	
+	public Customer(String customerName,String id) {
+		this.customerName = customerName;
+		this.customerId = id;
+	}
+
+}
+
